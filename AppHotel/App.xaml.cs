f8a9993cfe9445a5f18f16eff07a329f3402202e
LEONARDO <leonardo.goncales2@etec.sp.gov.br>
2025-06-01 14:18:05 -0300

    estilizacao da app

--- AppHotel/App.xaml.cs
+++ AppHotel/App.xaml.cs
@@ -6,7 +6,17 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 375;
+            window.Height = 800;
+
+            return window;
         }
     }
 }
--- AppHotel/MauiProgram.cs
+++ AppHotel/MauiProgram.cs
@@ -13,6 +13,8 @@ namespace AppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Cabin-Regular.ttf", "Cabin");
+                    fonts.AddFont("Cabin-SemiBold.ttf", "CabinSemiBold");
                 });
 
 #if DEBUG
